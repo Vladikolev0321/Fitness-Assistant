673f1660a8b066c1b7ce60a55f7cb55afd9d364e
Vladislav Kolev <vladi.kolev2003@gmail.com>
2021-12-08 23:12:10 +0200

    modified route /bot to accept POST request and respond with some information

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from fitness_assistant import FitnessAssistant
 
 app = Flask(__name__)
+fitness_assistant = FitnessAssistant()
 
-@app.route('/bot')
+
+@app.route('/bot', methods=["POST"])
 def bot():
-    return "Hello world"
+    try:
+        query = request.get_json()["query"]
+        result = None
+        if query == "steps-today":
+            result = fitness_assistant.get_steps_done_today()
+        elif query == "calories-burned-today":
+            result = fitness_assistant.get_calories_burned_today()
+        elif query == "avg-speed":
+            pass
+        elif query == "get-distance-changed":
+            pass
+        return str(result)
+    except:
+        pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
