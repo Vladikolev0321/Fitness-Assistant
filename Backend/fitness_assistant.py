0421d42b7a7b09aa2ec86f48820d3c0420ddb908
Vladislav Kolev <vladi.kolev2003@gmail.com>
2021-12-08 22:40:16 +0200

    added FitnessAssistant class with functionality that uses strava and fitbit class

---
+++ Backend/fitness_assistant.py
@@ -0,0 +1,19 @@
+from fitbit_api import FitbitApi
+from strava_api import StravaApi
+
+class FitnessAssistant:
+    def __init__(self):
+        self.__strava_api = StravaApi()
+        self.__fitbit_api = FitbitApi()
+
+    def get_steps_done_today(self):
+        return self.__fitbit_api.get_steps_done_today()
+
+    def get_calories_burned_today(self):
+        return self.__fitbit_api.get_calories_burned_today()
+
+    def get_average_speed_for_type_activity(self, type_activity):
+        return self.__strava_api.get_average_speed_for_type_activity(type_activity)
+
+    def get_distance_made_change_in_percents_for_type_activity(self, type_activity):
+        return self.__strava_api.get_distance_made_change_in_percents(type_activity)
