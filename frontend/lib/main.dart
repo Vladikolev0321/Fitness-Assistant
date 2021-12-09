7553e7ea2fcfd871cd106129ee24a7a9d6efd4a4
Vladislav Kolev <vladi.kolev2003@gmail.com>
2021-12-09 23:02:48 +0200

    created flutter app and started doing welcome screen

---
+++ frontend/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Fitness Assistant",
+      home: WelcomeScreen()
+    );
+  }
+}
