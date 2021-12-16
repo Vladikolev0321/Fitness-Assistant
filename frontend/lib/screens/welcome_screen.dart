68efb8a72a4494a34568299a77232170e5fd8a48
Vladislav Kolev <vladi.kolev2003@gmail.com>
2021-12-16 21:02:19 +0200

    changed welcome page

--- frontend/lib/screens/welcome_screen.dart
+++ frontend/lib/screens/welcome_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fitbitter/fitbitter.dart';
-import 'package:frontend/google_sign_in._api.dart';
+import 'package:frontend/google_sign_in_api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:strava_flutter/strava.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
+
 import '../secret.dart';
+import 'logged_in_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
@@ -15,11 +17,30 @@ class WelcomeScreen extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                SignInButton(
-                  Buttons.Google,
-                  text: "Sign up with Google",
-                  onPressed: signInWithGoogle,
+                Spacer(flex:1),
+                Image.asset("assets/chatbot.png",width:300,height:300),
+                Spacer(flex:1),
+                Text(
+                "Welcome to my \nfitness assistant app",
+                style: 
+                  Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              
+              Spacer(flex: 1),
+                Container(
+                  child: SignInButton(
+                    
+                    Buttons.Google,
+                    text: "Sign up with Google",
+                    onPressed: () => signInWithGoogle(context),
+                    
+                  ),
                 ),
+                Spacer(flex: 1,)
               ],
             ),
           ),
@@ -30,9 +51,23 @@ class WelcomeScreen extends StatelessWidget {
 }
 
 
-Future signInWithGoogle() async{
+Future signInWithGoogle(BuildContext context) async{
   final user = await GoogleSignInApi.login();
-  print(user.email);
+
+if(user != null){
+  
+    print(user.email);
+    print(user.displayName);
+    print(user.id);
+    
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => LoggedInPage(user: user),
+      ));
+  }else{
+
+  }
+
+  
 }
 
 class ConnectionWithApis extends StatelessWidget {
