b8b600b1d6704a634745170c5ed89c471a81b485
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-02-01 11:50:32 +0200

    Updated main file to work with Google sign in provider

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,18 +1,25 @@
-
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'providers/google_sign_in.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Fitness Assistant",
-      home: WelcomeScreen()
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Fitness Assistant",
+        home: WelcomeScreen()
+      ),
     );
   }
 }
