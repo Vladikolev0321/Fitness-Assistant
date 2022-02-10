b53aef75d924f7bd2c79e6060fd9c4120babbbac
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-02-10 22:11:43 +0200

    Added the distance widgets next to the activity pie chart

--- frontend/lib/widgets/pie_chart.dart
+++ frontend/lib/widgets/pie_chart.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:frontend/widgets/distance_widget.dart';
 
 class ActivityPieChart extends StatefulWidget {
   double rideSection;
   double runSection;
   double walkSection;
   double hikeSection;
+  int runDistance = 0;
+  int rideDistance = 0;
+  int walkDistance = 0;
+  int hikeDistance = 0;
 
-  ActivityPieChart({ Key key, this.rideSection, this.runSection, this.walkSection, this.hikeSection }) : super(key: key);
-
+  ActivityPieChart(
+      {Key key,
+      this.rideSection,
+      this.runSection,
+      this.walkSection,
+      this.hikeSection,
+      this.runDistance,
+      this.walkDistance,
+      this.rideDistance,
+      this.hikeDistance})
+      : super(key: key);
 
   @override
   _ActivityPieChartState createState() => _ActivityPieChartState();
@@ -19,18 +33,24 @@ class _ActivityPieChartState extends State<ActivityPieChart> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-        child: Row(
-          children: [
+        height: 250,
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+          child: Row(children: [
             Container(
+              width: 200,
               child: PieChart(
                 PieChartData(
                   borderData: FlBorderData(show: false),
                   centerSpaceRadius: 50.0,
                   sectionsSpace: 0.0,
                   startDegreeOffset: 30,
-                  sections: showingSections(widget.rideSection, widget.runSection, widget.walkSection, widget.hikeSection),
+                  sections: showingSections(
+                      widget.rideSection,
+                      widget.runSection,
+                      widget.walkSection,
+                      widget.hikeSection),
                   pieTouchData: PieTouchData(
                     touchCallback: (pieTouchResponse) {
                       setState(() {
@@ -46,13 +66,47 @@ class _ActivityPieChartState extends State<ActivityPieChart> {
                 ),
               ),
             ),
-          ]
-      ),
-    )
-    );
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 50),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    DistanceForActvity(
+                      color: Color(0xffF3BBEC),
+                      typeActivity: 'run',
+                      title: 'RUNNING',
+                      subtitle: '${widget.runDistance} KM',
+                    ),
+                    DistanceForActvity(
+                      color: Color(0xff39439f),
+                      typeActivity: 'ride',
+                      title: 'CYCLING',
+                      subtitle: '${widget.rideDistance} KM',
+                    ),
+                    DistanceForActvity(
+                      color: Color(0xff0eaeb4),
+                      typeActivity: 'walk',
+                      title: 'WALKING',
+                      subtitle: '${widget.walkDistance} KM',
+                    ),
+                    DistanceForActvity(
+                      color: Color(0xff0eae21),
+                      typeActivity: 'hike',
+                      title: 'HIKING',
+                      subtitle: '${widget.hikeDistance} KM',
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ));
   }
 
-  List<PieChartSectionData> showingSections(rideSection, runSection, walkSection, hikeSection) {
+  List<PieChartSectionData> showingSections(
+      rideSection, runSection, walkSection, hikeSection) {
     return List.generate(4, (i) {
       final isTouched = i == _touchedIndex;
       final double radius = isTouched ? 30 : 20;
@@ -68,19 +122,19 @@ class _ActivityPieChartState extends State<ActivityPieChart> {
           return PieChartSectionData(
               color: Color(0xffF3BBEC),
               value: runSection,
-              title: 'Run', 
+              title: 'Run',
               radius: radius);
         case 2:
           return PieChartSectionData(
               color: Color(0xff0eaeb4),
               value: walkSection,
-              title: 'Walk', 
+              title: 'Walk',
               radius: radius);
         case 3:
           return PieChartSectionData(
               color: Color(0xff0eae21),
               value: hikeSection,
-              title: 'Hike', 
+              title: 'Hike',
               radius: radius);
         default:
           return null;
