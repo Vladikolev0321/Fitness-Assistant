5f1eef7247aae717276dcf6346f58106b6c08e7d
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-02-17 13:38:41 +0200

    Added the StravaFitbitProvider to be consumed by FitnessInfoProvider

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/providers/fitness_api.dart';
+import 'package:frontend/providers/strava_fitbit.dart';
 import 'package:frontend/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'providers/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  GetIt locator = GetIt.instance;
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  locator.registerSingleton<SharedPreferences>(prefs);
   runApp(MyApp());
 }
 
@@ -17,7 +22,10 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<GoogleSignInProvider>(create: (context) => GoogleSignInProvider()),
-        Provider<FitnessInfoProvider>(create: (context) => FitnessInfoProvider())
+        Provider<StravaFitbitProvider>(create: (context) => StravaFitbitProvider()),
+        ProxyProvider<StravaFitbitProvider, FitnessInfoProvider>(
+          update: (context, stravaFitbit, _) => FitnessInfoProvider(stravaFitbit),
+          )
       ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
