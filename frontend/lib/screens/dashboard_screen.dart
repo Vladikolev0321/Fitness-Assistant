31c7715da3a9893c96680ff1f42b1bf448d18905
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-02 08:22:16 +0200

    Making request to server before loading dashboard screen

--- frontend/lib/screens/dashboard_screen.dart
+++ frontend/lib/screens/dashboard_screen.dart
@@ -7,155 +7,170 @@ import 'package:frontend/screens/steps_chart_screen.dart';
 import 'package:frontend/widgets/pie_chart.dart';
 import 'package:provider/provider.dart';
 
-
 class Dashboard extends StatefulWidget {
-  const Dashboard({Key key}) : super(key: key);
+  int stepsCount = 0;
+  int caloriesBurned = 0;
+  double runPercentage = 0;
+  double ridePercentage = 0;
+  double walkPercentage = 0;
+  double hikePercentage = 0;
+  int runDistance = 0;
+  int rideDistance = 0;
+  int walkDistance = 0;
+  int hikeDistance = 0;
+  double weight = 0;
+  Dashboard(
+      {Key key,
+      this.stepsCount,
+      this.caloriesBurned,
+      this.runPercentage,
+      this.ridePercentage,
+      this.walkPercentage,
+      this.hikePercentage,
+      this.runDistance,
+      this.rideDistance,
+      this.walkDistance,
+      this.hikeDistance,
+      this.weight})
+      : super(key: key);
+
   @override
   _DashboardState createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  int _stepsCount = 0;
-  int _caloriesBurned = 0;
-  double _runPercentage = 0;
-  double _ridePercentage = 0;
-  double _walkPercentage = 0;
-  double _hikePercentage = 0;
-  int _runDistance = 0;
-  int _rideDistance = 0;
-  int _walkDistance = 0;
-  int _hikeDistance = 0;
-  double _weight = 0;
   bool _isLoading = false;
   final _user = FirebaseAuth.instance.currentUser;
-  
+
   @override
   Widget build(BuildContext context) {
     return _isLoading
         ? Center(child: CircularProgressIndicator())
         : RefreshIndicator(
-          onRefresh: _getDashboardInfo,
-          child: SingleChildScrollView(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height: 10.0),
-                  Container(
-                    //padding: EdgeInsets.only(left: 20),
-                    child: Center(
-                      child: Text(
-                        "Hello, ${_user.displayName}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          fontSize: 20
-                        ),),
+            onRefresh: _getDashboardInfo,
+            child: SingleChildScrollView(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 10.0),
+                    Container(
+                      //padding: EdgeInsets.only(left: 20),
+                      child: Center(
+                        child: Text(
+                          "Hello, ${_user.displayName}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, fontSize: 20),
+                        ),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10.0),
-                  ActivityPieChart(
-                    runSection: _runPercentage,
-                    rideSection: _ridePercentage,
-                    walkSection: _walkPercentage,
-                    hikeSection: _hikePercentage,
-                    runDistance: _runDistance,
-                    rideDistance: _rideDistance,
-                    walkDistance: _walkDistance,
-                    hikeDistance: _hikeDistance,
-                  ),
-                  const SizedBox(height: 10.0),
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        height: 100,
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => StepsChartScreen()));
-                          },
+                    SizedBox(height: 10.0),
+                    ActivityPieChart(
+                      runSection: widget.runPercentage,
+                      rideSection: widget.ridePercentage,
+                      walkSection: widget.walkPercentage,
+                      hikeSection: widget.hikePercentage,
+                      runDistance: widget.runDistance,
+                      rideDistance: widget.rideDistance,
+                      walkDistance: widget.walkDistance,
+                      hikeDistance: widget.hikeDistance,
+                    ),
+                    const SizedBox(height: 10.0),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          height: 100,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          StepsChartScreen()));
+                            },
+                            child: Card(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(
+                                      widget.stepsCount.toString(),
+                                    ),
+                                    trailing: Icon(
+                                      FontAwesomeIcons.walking,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 16.0),
+                                    child: Text(
+                                      'Steps',
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        Container(
+                          height: 100,
                           child: Card(
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 ListTile(
-                                  title: Text(
-                                    _stepsCount.toString(),
-                                  ),
+                                  title: Text(widget.caloriesBurned.toString()),
                                   trailing: Icon(
-                                    FontAwesomeIcons.walking,
-                                    color: Colors.black,
+                                    FontAwesomeIcons.fire,
+                                    color: Colors.red,
                                   ),
                                 ),
                                 Padding(
                                   padding: const EdgeInsets.only(left: 16.0),
                                   child: Text(
-                                    'Steps',
+                                    'Calories Burned',
                                   ),
                                 )
                               ],
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Container(
-                        height: 100,
-                        child: Card(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(_caloriesBurned.toString()),
-                                trailing: Icon(
-                                  FontAwesomeIcons.fire,
-                                  color: Colors.red,
+                        const SizedBox(height: 10.0),
+                        Container(
+                          height: 100,
+                          child: Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                ListTile(
+                                  title: Text(widget.weight.toString()),
+                                  trailing: Icon(
+                                    FontAwesomeIcons.weight,
+                                    color: Colors.blue,
+                                  ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 16.0),
-                                child: Text(
-                                  'Calories Burned',
-                                ),
-                              )
-                            ],
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 16.0),
+                                  child: Text(
+                                    'Current weight',
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Container(
-                        height: 100,
-                        child: Card(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(_weight.toString()),
-                                trailing: Icon(
-                                  FontAwesomeIcons.weight,
-                                  color: Colors.blue,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 16.0),
-                                child: Text(
-                                  'Current weight',
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      
-                    ],
-                  ),
-                ],
-              )),
-        );
+                      ],
+                    ),
+                  ],
+                )),
+          );
   }
 
   @override
   void initState() {
     super.initState();
-    _getDashboardInfo();
+    //_getDashboardInfo();
   }
 
   Future<void> _getDashboardInfo() async {
@@ -163,24 +178,25 @@ class _DashboardState extends State<Dashboard> {
       _isLoading = true;
     });
     Map<String, dynamic> data;
-      await Provider.of<FitnessInfoProvider>(context, listen: false)
-            .getDashboardInfo()
-            .then((response) {
+    await Provider.of<FitnessInfoProvider>(context, listen: false)
+        .getDashboardInfo()
+        .then((response) {
       data = jsonDecode(response.body);
+      print(data);
     });
     setState(() {
       /// Check if contains keys
-      _stepsCount = int.parse(data['steps']);
-      _caloriesBurned = int.parse(data['burnt_calories']);
-      _runPercentage = data['percentages']['run_distance_percentage'];
-      _ridePercentage = data['percentages']['ride_distance_percentage'];
-      _walkPercentage = data['percentages']['walk_distance_percentage'];
-      _hikePercentage = data['percentages']['hike_distance_percentage'];
-      _runDistance = data['distances']['run_distance'];
-      _rideDistance = data['distances']['ride_distance'];
-      _walkDistance = data['distances']['walk_distance'];
-      _hikeDistance = data['distances']['hike_distance'];
-      _weight = double.parse(data['weight'].toStringAsFixed(2));
+      widget.stepsCount = int.parse(data['steps']);
+      widget.caloriesBurned = int.parse(data['burnt_calories']);
+      widget.runPercentage = data['percentages']['run_distance_percentage'];
+      widget.ridePercentage = data['percentages']['ride_distance_percentage'];
+      widget.walkPercentage = data['percentages']['walk_distance_percentage'];
+      widget.hikePercentage = data['percentages']['hike_distance_percentage'];
+      widget.runDistance = data['distances']['run_distance'];
+      widget.rideDistance = data['distances']['ride_distance'];
+      widget.walkDistance = data['distances']['walk_distance'];
+      widget.hikeDistance = data['distances']['hike_distance'];
+      widget.weight = double.parse(data['weight'].toStringAsFixed(2));
       _isLoading = false;
     });
   }
--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:frontend/screens/chat_screen.dart';
 import 'package:frontend/screens/user_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/fitness_api.dart';
 import 'dashboard_screen.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,12 +16,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int stepsCount = 0;
+  int caloriesBurned = 0;
+  double runPercentage = 0;
+  double ridePercentage = 0;
+  double walkPercentage = 0;
+  double hikePercentage = 0;
+  int runDistance = 0;
+  int rideDistance = 0;
+  int walkDistance = 0;
+  int hikeDistance = 0;
+  double weight = 0;
   int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    Dashboard(),
-    ChatBody(),
-    UserScreen()
-  ];
+  bool _isLoading = false;
+
+  
 
   void _onItemTapped(int index) {
     setState(() {
@@ -27,7 +40,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    List<Widget> _widgetOptions = <Widget>[
+    Dashboard(
+      stepsCount: stepsCount,
+      caloriesBurned: caloriesBurned,
+      runPercentage: runPercentage ,
+      ridePercentage: ridePercentage,
+      walkPercentage: walkPercentage,
+      hikePercentage: hikePercentage,
+      runDistance: runDistance,
+      rideDistance: rideDistance,
+      walkDistance: walkDistance,
+      hikeDistance: hikeDistance,
+      weight: weight,
+    ),
+    ChatBody(),
+    UserScreen()
+  ];
+
+    return _isLoading
+        ? Center(child: CircularProgressIndicator())
+      : Scaffold(
       body: Center(
          child:  _widgetOptions.elementAt(_selectedIndex),
 
@@ -54,5 +87,39 @@ class _HomePageState extends State<HomePage> {
   ),
   
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getDashboardInfo();
+  }
+
+
+  Future<void> _getDashboardInfo() async {
+    setState(() {
+      _isLoading = true;
+    });
+    Map<String, dynamic> data;
+    await Provider.of<FitnessInfoProvider>(context, listen: false)
+        .getDashboardInfo()
+        .then((response) {
+      data = jsonDecode(response.body);
+    });
+    setState(() {
+      /// Check if contains keys
+      stepsCount = int.parse(data['steps']);
+      caloriesBurned = int.parse(data['burnt_calories']);
+      runPercentage = data['percentages']['run_distance_percentage'];
+      ridePercentage = data['percentages']['ride_distance_percentage'];
+      walkPercentage = data['percentages']['walk_distance_percentage'];
+      hikePercentage = data['percentages']['hike_distance_percentage'];
+      runDistance = data['distances']['run_distance'];
+      rideDistance = data['distances']['ride_distance'];
+      walkDistance = data['distances']['walk_distance'];
+      hikeDistance = data['distances']['hike_distance'];
+      weight = double.parse(data['weight'].toStringAsFixed(2));
+      _isLoading = false;
+    });
   }
 }
