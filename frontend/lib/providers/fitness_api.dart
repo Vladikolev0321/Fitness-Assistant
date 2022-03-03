560f27f351604a5074b8d12b864a2926be6668bc
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-03 10:48:06 +0200

    Added dashboard info model to the provider class

--- frontend/lib/providers/fitness_api.dart
+++ frontend/lib/providers/fitness_api.dart
@@ -1,36 +1,56 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:frontend/models/dashboard_info.dart';
 import 'package:frontend/providers/strava_fitbit.dart';
 import 'package:frontend/secret.dart';
 import 'package:http/http.dart' as http;
 
-class FitnessInfoProvider{
+class FitnessInfoProvider {
+  final _user = FirebaseAuth.instance.currentUser;
+  StravaFitbitProvider _stravaFitbitProvider;
 
-    final _user = FirebaseAuth.instance.currentUser;
-    StravaFitbitProvider _stravaFitbitProvider;
+  FitnessInfoProvider(this._stravaFitbitProvider);
 
-    FitnessInfoProvider(this._stravaFitbitProvider);
+  DashboardInfo _dashboardInfo;
 
-    Future<http.Response> getDashboardInfo()async {
-       final idToken = await _user.getIdToken();
-      final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
-      final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
+  DashboardInfo get dashboardInfo => _dashboardInfo;
 
-      final http.Response response = await http.get(Uri.parse("$baseUrl/dashboard"),
-                           headers: {'Content-type': 'application/json', "Authorization":idToken,
-                          "stravaAccessToken":stravaTokens['stravaAccessToken'], "stravaRefreshToken":stravaTokens['stravaRefreshToken'],
-                           "fitbitAccessToken":fitbitTokens['fitbitAccessToken'],"fitbitRefreshToken":fitbitTokens['fitbitRefreshToken']});
-      return response;
-    }
+  set dashboardInfo(DashboardInfo dashboardInfo) {
+    _dashboardInfo = dashboardInfo;
+  }
 
-    Future<http.Response> getStepsInfo()async {
-       final idToken = await _user.getIdToken();
-      final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
-      final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
 
-      final http.Response response = await http.get(Uri.parse("$baseUrl/dashboard/steps_chart"),
-                           headers: {'Content-type': 'application/json', "Authorization":idToken,
-                          "stravaAccessToken":stravaTokens['stravaAccessToken'], "stravaRefreshToken":stravaTokens['stravaRefreshToken'],
-                           "fitbitAccessToken":fitbitTokens['fitbitAccessToken'],"fitbitRefreshToken":fitbitTokens['fitbitRefreshToken']});
-      return response;
-    }
+
+  Future<http.Response> getDashboardInfo() async {
+    final idToken = await _user.getIdToken();
+    final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
+    final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
+
+    final http.Response response =
+        await http.get(Uri.parse("$baseUrl/dashboard"), headers: {
+      'Content-type': 'application/json',
+      "Authorization": idToken,
+      "stravaAccessToken": stravaTokens['stravaAccessToken'],
+      "stravaRefreshToken": stravaTokens['stravaRefreshToken'],
+      "fitbitAccessToken": fitbitTokens['fitbitAccessToken'],
+      "fitbitRefreshToken": fitbitTokens['fitbitRefreshToken']
+    });
+    return response;
+  }
+
+  Future<http.Response> getStepsInfo() async {
+    final idToken = await _user.getIdToken();
+    final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
+    final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
+
+    final http.Response response =
+        await http.get(Uri.parse("$baseUrl/dashboard/steps_chart"), headers: {
+      'Content-type': 'application/json',
+      "Authorization": idToken,
+      "stravaAccessToken": stravaTokens['stravaAccessToken'],
+      "stravaRefreshToken": stravaTokens['stravaRefreshToken'],
+      "fitbitAccessToken": fitbitTokens['fitbitAccessToken'],
+      "fitbitRefreshToken": fitbitTokens['fitbitRefreshToken']
+    });
+    return response;
+  }
 }
