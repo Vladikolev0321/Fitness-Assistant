a87ed8516561a1686dc5e3fcfc9e31b4a92eb1c9
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-03 13:18:05 +0200

    Added messages list to the Messages class with adding of messages function

--- frontend/lib/providers/messages.dart
+++ frontend/lib/providers/messages.dart
@@ -1,36 +1,51 @@
 import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:frontend/models/chat_message.dart';
 import 'package:frontend/providers/strava_fitbit.dart';
 import 'package:frontend/secret.dart';
 import 'package:http/http.dart' as http;
 
-class Messages{
-
+class Messages {
   final _user = FirebaseAuth.instance.currentUser;
-    StravaFitbitProvider _stravaFitbitProvider;
+  StravaFitbitProvider _stravaFitbitProvider;
+  List<ChatMessage> _messages = [];
 
-    Messages(this._stravaFitbitProvider);
+  Messages(this._stravaFitbitProvider);
 
-    Future<String> message(String message) async {
-      final idToken = await _user.getIdToken();
-      final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
-      final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
+  List<ChatMessage> get messages => _messages;
 
-      http.Client client = getClient();
-      final response = await client.post(Uri.parse("$baseUrl/bot/message"),
-        headers: {'Content-type': 'application/json', "Authorization":idToken,
-        "stravaAccessToken":stravaTokens['stravaAccessToken'], "stravaRefreshToken":stravaTokens['stravaRefreshToken'],
-        "fitbitAccessToken":fitbitTokens['fitbitAccessToken'],"fitbitRefreshToken":fitbitTokens['fitbitRefreshToken']},
-        body: json.encode({"message": message}),
-      );
+  void addMessage(String message, bool isSender){
+    _messages.add(ChatMessage(
+        text: message,
+        messageType: ChatMessageType.text,
+        isSender: isSender ? true : false,));
+  }
 
-        Map<String, dynamic> data = jsonDecode(response.body);
-        return data['response'];
-    }
+  Future<String> message(String message) async {
+    final idToken = await _user.getIdToken();
+    final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
+    final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
 
-    http.Client getClient(){
-      return http.Client();
-    }
+    http.Client client = getClient();
+    final response = await client.post(
+      Uri.parse("$baseUrl/bot/message"),
+      headers: {
+        'Content-type': 'application/json',
+        "Authorization": idToken,
+        "stravaAccessToken": stravaTokens['stravaAccessToken'],
+        "stravaRefreshToken": stravaTokens['stravaRefreshToken'],
+        "fitbitAccessToken": fitbitTokens['fitbitAccessToken'],
+        "fitbitRefreshToken": fitbitTokens['fitbitRefreshToken']
+      },
+      body: json.encode({"message": message}),
+    );
 
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data['response'];
+  }
+
+  http.Client getClient() {
+    return http.Client();
+  }
 }
