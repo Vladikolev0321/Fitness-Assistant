eef268c43a80cd34fc45ea792d1c81c12164ccb6
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-05 21:36:59 +0200

    Added a activities screen that will show average change in speed of activity

---
+++ frontend/lib/screens/activities_screen.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/models/activity_average_data.dart';
+import 'package:frontend/widgets/chart_container.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/fitness_api.dart';
+import '../widgets/line_chart.dart';
+
+class ActivityScreen extends StatefulWidget {
+  const ActivityScreen({ Key key }) : super(key: key);
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final runLineChartBarData = Provider.of<FitnessInfoProvider>(context, listen: false).runLineChartBarData;
+    print(runLineChartBarData);
+    return Scaffold(
+      appBar: AppBar(),
+      body:Center(
+        child: Container(
+          padding: const EdgeInsets.all(15.0),
+          height: 300,
+            child: PageView(
+              children: [
+                  ChartContainer(
+                    title: 'Running average speed change', 
+                    color: Color.fromRGBO(45, 108, 223, 1), 
+                    chart: LineChartContent(lineChartBarData: runLineChartBarData)
+                  ), 
+                Container(
+                  child: Center(child: Text("Page2")),
+                )
+              ],
+            ),
+          
+        ),
+      )
+    );
+  }
+
+  Future<void> _getActivityAverages() async{
+    final response = await Provider.of<FitnessInfoProvider>(context, listen: false).getActivitiesAverages();
+    Map<String, dynamic> responseBody = jsonDecode(response.body);
+    final List<double> yValues = responseBody['average_speed_list']['runs_average'].cast<double>();;
+    List<FlSpot> spots =  yValues.asMap().entries.map((e) {
+         return FlSpot(e.key.toDouble(), e.value);
+      }).toList();
+      
+      List<Color> lineColor = [
+          Color(0xfff3f169),
+      ];
+
+      List<LineChartBarData> lineChartBarData = [
+        LineChartBarData(
+          colors: lineColor,
+          isCurved: true,
+          spots: spots
+        )
+      ];
+
+      Provider.of<FitnessInfoProvider>(context, listen: false).runLineChartBarData = lineChartBarData;
+    
+  }
+}
