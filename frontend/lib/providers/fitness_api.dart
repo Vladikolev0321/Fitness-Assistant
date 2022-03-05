42b554f9ba44964354943cf6b2d025b1e2a15a3e
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-05 21:47:54 +0200

    Modified fitness info provider class
    * added method for getting average of activities 
    * line chart bar members

--- frontend/lib/providers/fitness_api.dart
+++ frontend/lib/providers/fitness_api.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:frontend/models/dashboard_info.dart';
 import 'package:frontend/providers/strava_fitbit.dart';
 import 'package:frontend/secret.dart';
@@ -11,12 +12,23 @@ class FitnessInfoProvider {
   FitnessInfoProvider(this._stravaFitbitProvider);
 
   DashboardInfo _dashboardInfo;
+  List<LineChartBarData> _runLineChartBarData;
+  List<LineChartBarData> _walkLineChartBarData;
+  List<LineChartBarData> _hikeLineChartBarData;
+  List<LineChartBarData> _rideLineChartBarData;
+
+  List<LineChartBarData> get runLineChartBarData => _runLineChartBarData;
+
+  set runLineChartBarData(List<LineChartBarData> runLineChartBarData) {
+    _runLineChartBarData = runLineChartBarData;
+  }
 
   DashboardInfo get dashboardInfo => _dashboardInfo;
 
   set dashboardInfo(DashboardInfo dashboardInfo) {
     _dashboardInfo = dashboardInfo;
   }
+
 
 
 
@@ -44,6 +56,23 @@ class FitnessInfoProvider {
 
     final http.Response response =
         await http.get(Uri.parse("$baseUrl/dashboard/steps_chart"), headers: {
+      'Content-type': 'application/json',
+      "Authorization": idToken,
+      "stravaAccessToken": stravaTokens['stravaAccessToken'],
+      "stravaRefreshToken": stravaTokens['stravaRefreshToken'],
+      "fitbitAccessToken": fitbitTokens['fitbitAccessToken'],
+      "fitbitRefreshToken": fitbitTokens['fitbitRefreshToken']
+    });
+    return response;
+  }
+
+  Future<http.Response> getActivitiesAverages() async {
+    final idToken = await _user.getIdToken();
+    final stravaTokens = await _stravaFitbitProvider.getStravaTokens();
+    final fitbitTokens = await _stravaFitbitProvider.getFitbitTokens();
+
+    final http.Response response =
+        await http.get(Uri.parse("$baseUrl/dashboard/activities_average"), headers: {
       'Content-type': 'application/json',
       "Authorization": idToken,
       "stravaAccessToken": stravaTokens['stravaAccessToken'],
