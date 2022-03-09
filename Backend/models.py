6985ab1d2dc7689e651860302541a988c8902bda
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-09 22:29:30 +0200

    Added user data and fitness tokens model
    * user data - fitness activity
    * fitness tokens - strava and fitbit auth tokens

--- Backend/models.py
+++ Backend/models.py
@@ -15,9 +15,32 @@ class NotUnderstoodQuestion(db.Model):
         self.text = text
 
 class UserData(db.Model):
+    __table_args__ = (
+        db.UniqueConstraint('user_id', 'date', name='unique_user_id_date'),
+    )
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     steps_count = db.Column(db.Integer, nullable=False)
     calories_out = db.Column(db.Integer, nullable=False)
+    
+    def __init__(self, user_id, date, steps_count, calories_out):
+        self.user_id = user_id
+        self.date = date
+        self.steps_count = steps_count
+        self.calories_out = calories_out
+
+
+class FitnessTokens(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), unique=True, nullable=False)
+    strava_refresh_token = db.Column(db.Text(), nullable=False)
+    fitbit_access_token = db.Column(db.Text(), nullable=False)
+    fitbit_refresh_token = db.Column(db.Text(), nullable=False)
+
+    def __init__(self, user_id, strava_refresh_token, fitbit_access_token, fitbit_refresh_token):
+        self.user_id = user_id
+        self.strava_refresh_token = strava_refresh_token
+        self.fitbit_access_token = fitbit_access_token
+        self.fitbit_refresh_token = fitbit_refresh_token
 
