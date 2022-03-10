c044a8d3b0bb70761303b17302ecfd4ec65437a5
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-10 22:16:54 +0200

    updated line chart

--- frontend/lib/widgets/line_chart.dart
+++ frontend/lib/widgets/line_chart.dart
@@ -3,18 +3,26 @@ import 'package:flutter/material.dart';
 
 class LineChartContent extends StatelessWidget {
   List<LineChartBarData> lineChartBarData;
-  LineChartContent({@required this.lineChartBarData});
+  double minX;
+  double maxX;
+  double minY;
+  double maxY;
+  bool displayY;
+  LineChartContent({
+    @required this.lineChartBarData,
+    @required this.minX,
+    @required this.maxX,
+    @required this.minY,
+    @required this.maxY,
+    @required this.displayY,
+  });
 
   @override
   Widget build(BuildContext context) {
     return LineChart(
       LineChartData(
-        borderData: FlBorderData(
-          border: Border.all(
-            color: Colors.white, 
-            width: 0.5
-          )
-        ),
+        borderData:
+            FlBorderData(border: Border.all(color: Colors.white, width: 0.5)),
         gridData: FlGridData(
           drawHorizontalLine: false,
         ),
@@ -22,10 +30,7 @@ class LineChartContent extends StatelessWidget {
           bottomTitles: SideTitles(
             showTitles: true,
             textStyle: TextStyle(
-                color: Colors.white, 
-                fontSize: 12, 
-                fontWeight: FontWeight.bold
-              ),
+                color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold),
             getTitles: (value) {
               // if(value.toInt() == 0) return '';
               // else return value.toInt().toString();
@@ -36,20 +41,22 @@ class LineChartContent extends StatelessWidget {
             interval: 4,
             showTitles: true,
             textStyle: TextStyle(
-                color: Colors.white, 
-                fontSize: 14, 
-                fontWeight: FontWeight.bold
-              ),
+                color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
             getTitles: (value) {
-              if(value.toInt() == 0) return '';
-              else return value.toInt().toString();
+              if(displayY){
+                if(value.toInt() == 0) return '';
+                else return value.toInt().toString();
+              }else{
+                return '';
+              }
+             // return '';
             },
           ),
         ),
-        minX: 1,
-        minY: 0,
-        maxX: 10,
-        maxY: 20,
+        minX: minX,
+        minY: minY,
+        maxX: maxX,
+        maxY: maxY,
         lineBarsData: lineChartBarData,
       ),
     );
