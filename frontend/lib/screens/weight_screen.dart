73101f59538da2951b17abb5a0680c3493c0cd8d
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-11 22:44:40 +0200

    Added weight info screen

---
+++ frontend/lib/screens/weight_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/models/activity_average_data.dart';
+import 'package:frontend/widgets/chart_container.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/fitness_api.dart';
+import '../widgets/line_chart.dart';
+
+class WeightScreen extends StatefulWidget {
+  const WeightScreen({ Key key }) : super(key: key);
+
+  @override
+  State<WeightScreen> createState() => _WeightScreenState();
+}
+
+class _WeightScreenState extends State<WeightScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final weightLineChartBarData = Provider.of<FitnessInfoProvider>(context, listen: false).weightLineChartBarData;
+
+    return Scaffold(
+      appBar: AppBar(),
+      body:Center(
+        child: Container(
+          padding: const EdgeInsets.all(15.0),
+          height: 300,
+            child: ChartContainer(
+                    title: 'Last logged weights', 
+                    color: Color.fromRGBO(45, 108, 223, 1), 
+                    chart: LineChartContent(lineChartBarData: weightLineChartBarData, minX: 1, minY: 0, maxX:10, maxY:200, displayY: true, intervalY: 20,)
+                  ), 
+        ),
+      )
+    );
+  }
+}
