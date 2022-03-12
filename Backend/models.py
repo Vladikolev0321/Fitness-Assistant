e34a32d6448fb08c01b11147fe9003e56a5a3749
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-12 22:41:15 +0200

    Added Strava activities and Weight db models

--- Backend/models.py
+++ Backend/models.py
@@ -1,3 +1,6 @@
+from dis import dis
+from tracemalloc import start
+from turtle import distance
 from database import db
 
 class Profile(db.Model):
@@ -29,6 +32,39 @@ class UserData(db.Model):
         self.date = date
         self.steps_count = steps_count
         self.calories_out = calories_out
+
+class StravaActivities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    upload_id = db.Column(db.BigInteger, unique=True, nullable=False)
+    type = db.Column(db.String(80), nullable=False)
+    distance = db.Column(db.Integer, nullable=False)
+    moving_time = db.Column(db.Integer, nullable=False)
+    average_speed = db.Column(db.Float, nullable=False)
+    max_speed = db.Column(db.Float, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, user_id, upload_id, type, distance, moving_time, average_speed, max_speed, start_date):
+        self.user_id = user_id
+        self.upload_id = upload_id
+        self.type = type
+        self.distance = distance
+        self.moving_time = moving_time
+        self.average_speed = average_speed
+        self.max_speed = max_speed
+        self.start_date = start_date
+
+class Weight(db.Model):
+    id = id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+
+    def __init__(self, user_id, date, weight):
+        self.user_id = user_id
+        self.date = date
+        self.weight = weight
+
 
 
 class FitnessTokens(db.Model):
