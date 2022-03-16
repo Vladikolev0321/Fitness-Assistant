1104c283d18604c41ef5c6f390011d15d31da820
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-16 21:02:59 +0200

    Change some models columns names

--- Backend/models.py
+++ Backend/models.py
@@ -3,26 +3,28 @@ from tracemalloc import start
 from turtle import distance
 from database import db
 
-class Profile(db.Model):
+class Profiles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, email):
+        self.username = name
+        self.email = email
 
-class NotUnderstoodQuestion(db.Model):
+class NotUnderstoodQuestions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(80), nullable=False)
+    question = db.Column(db.String(80), nullable=False)
 
     def __init__(self, text):
-        self.text = text
+        self.question = text
 
-class UserData(db.Model):
+class CaloriesStepsData(db.Model):
     __table_args__ = (
         db.UniqueConstraint('user_id', 'date', name='unique_user_id_date'),
     )
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     steps_count = db.Column(db.Integer, nullable=False)
     calories_out = db.Column(db.Integer, nullable=False)
@@ -35,14 +37,14 @@ class UserData(db.Model):
 
 class StravaActivities(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), nullable=False)
     upload_id = db.Column(db.BigInteger, unique=True, nullable=False)
     type = db.Column(db.String(80), nullable=False)
     distance = db.Column(db.Integer, nullable=False)
     moving_time = db.Column(db.Integer, nullable=False)
     average_speed = db.Column(db.Float, nullable=False)
     max_speed = db.Column(db.Float, nullable=False)
-    start_date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, user_id, upload_id, type, distance, moving_time, average_speed, max_speed, start_date):
         self.user_id = user_id
@@ -52,11 +54,11 @@ class StravaActivities(db.Model):
         self.moving_time = moving_time
         self.average_speed = average_speed
         self.max_speed = max_speed
-        self.start_date = start_date
+        self.date = start_date
 
-class Weight(db.Model):
+class WeightsData(db.Model):
     id = id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     weight = db.Column(db.Float, nullable=False)
 
@@ -69,7 +71,7 @@ class Weight(db.Model):
 
 class FitnessTokens(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), unique=True, nullable=False)
     strava_refresh_token = db.Column(db.Text(), nullable=False)
     fitbit_access_token = db.Column(db.Text(), nullable=False)
     fitbit_refresh_token = db.Column(db.Text(), nullable=False)
