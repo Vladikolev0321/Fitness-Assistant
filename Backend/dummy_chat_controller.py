91ae470b695b615a2874bafd1980afef946509de
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-03-31 23:58:27 +0300

    Delete file

--- Backend/dummy_chat_controller.py
+++
@@ -1,8 +0,0 @@
-class DummyChat:
-    # def __init__(self, fitness_assistant):
-    #     self.__fitness_assist = fitness_assistant
-
-    def accept_message(self, fitbit_api, strava_api, message):
-        if(message == "steps?"):
-            # return self.__fitness_assist.get_steps_done_today()
-            return fitbit_api.get_steps_done_today()
